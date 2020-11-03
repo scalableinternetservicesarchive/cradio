ff8ff6e6480a51b6939b742d0e91d344211e51a0
Robert Chang <robertchang25@g.ucla.edu>
2020-11-02 22:44:29 -0800

    Artists/Songs 1

---
+++ server/src/db/migrations/Add_artists.sql
@@ -0,0 +1,5 @@
+insert into `artist` (`name`, `origin`) values ('Journey', 'United States');
+insert into `artist` (`name`, `origin`) values ('Green Day', 'United States');
+insert into `artist` (`name`, `origin`) values ('Bruno Mars', 'United States');
+insert into `artist` (`name`, `origin`) values ('Adele', 'United Kingdom');
+insert into `artist` (`name`, `origin`) values ('Enrique Iglesias', 'Spain');
---
+++ server/src/db/migrations/Add_songs.sql
@@ -0,0 +1,2 @@
+insert into `song` (`name`, `duration`, `genre`, `artistId`) values ('Faithfully', 240, 'Pop Rock' ,'1';
+
