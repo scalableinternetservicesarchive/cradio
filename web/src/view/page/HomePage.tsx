4cec91ea05b5b2668d7558195a98adc3d350eb93
Daniel Jaffe <daniel.jaffe@ucla.edu>
2020-11-13 20:15:12 -0800

    More derp but closer

--- web/src/view/page/HomePage.tsx
+++ web/src/view/page/HomePage.tsx
@@ -1,33 +1,76 @@
-import { RouteComponentProps } from '@reach/router'
+//import { useQuery } from '@apollo/client'
+import { navigate, RouteComponentProps } from '@reach/router'
 import * as React from 'react'
+import { Button, Form } from 'react-bootstrap'
 import { Colors } from '../../../../common/src/colors'
+//import { FetchListeningSession, FetchListeningSessionVariables } from '../../graphql/query.gen'
 import { H1, H3 } from '../../style/header'
 import { style } from '../../style/styled'
-import { AppRouteParams } from '../nav/route'
-import { Sessions } from '../playground/Sessions'
+import { AppRouteParams, getPath, Route } from '../nav/route'
+//import { fetchListeningSession } from '../playground/fetchListeningSession'
 import { Page } from './Page'
+// import { createPartyRocker } from '../playground/mutatePartyRockers'
+// import { createListeningSession } from '../playground/mutateListeningSession'
 
-interface HomePageProps extends RouteComponentProps, AppRouteParams {}
+interface HomePageProps extends RouteComponentProps, AppRouteParams { }
 
 export function HomePage(props: HomePageProps) {
-  return (
-    <Page>
-      <Hero>
-        <H1>Cradio</H1>
-        <H3>Start Listening Parties with you friends! Request Music!</H3>
-        <H3>UCLA, Fall 2020</H3>
-      </Hero>
-      <img src={"/app/assets/cradio.png"}/>
-      <Sessions />
-    </Page>
-  )
+	const [username, setUsername] = React.useState("");
+	const [sessionID, setSessionID] = React.useState("");
+
+
+
+	function joinSession(): void {
+		// <createPartyRocker/ >
+		//const { data } = useQuery<FetchListeningSession, FetchListeningSessionVariables>(fetchListeningSession, { sessionId: 1 })
+		const newURL = getPath(Route.LECTURES_NEW, { sessionID: 1 })
+		console.log(newURL)
+		navigate(newURL)
+	}
+
+	// function createSession() {
+	// 	// <createPartyRocker/ >
+	// 	// <createListeningSession/ >
+	// }
+
+	return (
+		<Page>
+			<Hero>
+				<H1>Cradio</H1>
+				<H3>Start Listening Parties with you friends! Request Music!</H3>
+				<H3>UCLA, Fall 2020</H3>
+				{username}
+				{sessionID}
+			</Hero>
+			{/* Logo here */}
+			<img src={"/app/assets/cradio.png"} />
+
+			<Form>
+				<Form.Group>
+					<Form.Label>Name</Form.Label>
+					<Form.Control type="text" placeholder="Username" onChange={(event) => setUsername(event.target.value)} />
+					<Form.Text className="text-muted">
+						Input your desired username.
+    				</Form.Text>
+				</Form.Group>
+
+				<Form.Group>
+					<Form.Label>Session ID</Form.Label>
+					<Form.Control type="text" placeholder="Session ID" onChange={(event) => setSessionID(event.target.value)} />
+				</Form.Group>
+				<Button variant="primary" type="submit" onClick={() => joinSession()} >
+					Submit
+       			</Button>
+			</Form>
+		</Page>
+	)
 }
 
 const Hero = style('div', 'mb4 w-100 ba b--mid-gray br2 pa3 tc', {
-  borderLeftColor: Colors.lemon + '!important',
-  borderRightColor: Colors.lemon + '!important',
-  borderLeftWidth: '4px',
-  borderRightWidth: '4px',
+	borderLeftColor: Colors.lemon + '!important',
+	borderRightColor: Colors.lemon + '!important',
+	borderLeftWidth: '4px',
+	borderRightWidth: '4px',
 })
 
 // this is a test comment
--- web/src/view/page/LecturesPage.tsx
+++ web/src/view/page/LecturesPage.tsx
@@ -1,22 +1,23 @@
 import { RouteComponentProps } from '@reach/router'
 import * as React from 'react'
-import { useState } from 'react'
-import ListGroup from 'react-bootstrap/ListGroup'
+// import { useState } from 'react'
 import { AppRouteParams } from '../nav/route'
 import { Page } from './Page'
 
 
-interface LecturesPageProps extends RouteComponentProps, AppRouteParams {}
+interface LecturesPageProps extends RouteComponentProps, AppRouteParams { }
 
 export function LecturesPage(props: LecturesPageProps) {
-  const [sessions] = useState(["Session 1", "Session 2", "Session 3"]);
+  // const [sessions] = useState(["Session 1", "Session 2", "Session 3"]);
   return (
     <Page>
       <div>
-        <ListGroup>
+        {/* <ListGroup>
           {sessions.map(id => (<ListGroup.Item>{id}</ListGroup.Item>))}
-        </ListGroup>
-        </div>
+        </ListGroup> */}
+        {props.sessionID}
+
+      </div>
     </Page>
   )
 }
