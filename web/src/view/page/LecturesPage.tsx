c36c73f9d1dc7485f26c79d93bd9e499e0a021d8
grantrbrts <groberts96@gmail.com>
2020-11-19 08:33:49 -0800

    Adding frontend for sessions

--- web/src/view/page/LecturesPage.tsx
+++ web/src/view/page/LecturesPage.tsx
@@ -32,10 +32,6 @@ const useStyles = makeStyles(theme => ({
 }))
 
 export function LecturesPage(props: LecturesPageProps) {
-  return <SongList />
-}
-
-function SongList() {
   const classes = useStyles()
   const [dense] = React.useState(false)
   const [secondary] = React.useState(false)
@@ -97,3 +93,66 @@ function SongList() {
     </Page>
   )
 }
+
+// function SongList() {
+//   const classes = useStyles()
+//   const [dense] = React.useState(false)
+//   const [secondary] = React.useState(false)
+//   const { loading, data } = useQuery<FetchSongs>(fetchSongs)
+//   const sessionData = useQuery<FetchListeningSession, FetchListeningSessionVariables>(fetchListeningSession, {
+//     variables: { sessionId: 4 },
+//   }).data
+
+//   console.log(data)
+//   console.log(sessionData)
+//   if (loading) {
+//     return <div>loading...</div>
+//   }
+//   if (!data || data.songs.length === 0) {
+//     return <div>no songs</div>
+//   }
+
+//   const list2 = ['queue1', 'queue2']
+//   return (
+//     <Page>
+//       <Grid container>
+//         <Grid item xs={12} md={6}>
+//           <Typography variant="h6" className={classes.title}>
+//             Songs
+//           </Typography>
+//           <div className={classes.demo}>
+//             <List dense={dense}>
+//               {data.songs.map(currSong => (
+//                 <ListItem>
+//                   <ListItemIcon>
+//                     <AddIcon
+//                       onClick={() => {
+//                         console.log(currSong)
+//                         addToQueue({ songId: currSong.id, listeningSessionId: 4 })
+//                       }}
+//                     />
+//                   </ListItemIcon>
+//                   <ListItemText primary={currSong.name} secondary={secondary ? 'Secondary text' : null} />
+//                 </ListItem>
+//               ))}
+//             </List>
+//           </div>
+//         </Grid>
+//         <Grid item xs={12} md={6}>
+//           <Typography variant="h6" className={classes.title}>
+//             Session Queue
+//           </Typography>
+//           <div className={classes.demo}>
+//             <List dense={dense}>
+//               {list2.map(currSong => (
+//                 <ListItem>
+//                   <ListItemText primary={currSong} secondary={secondary ? 'Secondary text' : null} />
+//                 </ListItem>
+//               ))}
+//             </List>
+//           </div>
+//         </Grid>
+//       </Grid>
+//     </Page>
+//   )
+// }
